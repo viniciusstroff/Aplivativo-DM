c78d12a84730a9a1a2c7f57e2f2354d3a021a848
Filipe Silva <filipe.silva.002@acad.pucrs.br>
2020-11-05 23:45:02 -0300

    Removendo gif da página de glicemia capilar

--- lib/Glicemia/GlicemiaCapilarPage.dart
+++ lib/Glicemia/GlicemiaCapilarPage.dart
@@ -10,22 +10,38 @@ class GlicemiaCapilarPage extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: <Widget>[
-          CustomSliverAppBar(title: "Glicemia Capilar",),
+          CustomSliverAppBar(
+            title: "Glicemia Capilar",
+          ),
           SliverList(
             delegate: SliverChildListDelegate(
               [
-                SimpleTextComponent(text:'A automonitorização da glicemia capilar (AMGC) é efetuada com a inserção de uma gota de sangue capilar em uma fita descartável acoplada a um dispositivo, o glicosímetro.  (GIF)'),
-                imageDetails2(context, 'assets/images/exercicio.gif', 'exercícios'),
+                SimpleTextComponent(
+                    text:
+                        'A automonitorização da glicemia capilar (AMGC) é efetuada com a inserção de uma gota de sangue capilar em uma fita descartável acoplada a um dispositivo, o glicosímetro.'),
+                //imageDetails2(context, 'assets/images/exercicio.gif', 'exercícios'),
                 // Image.asset("assets/images/exercicio.gif"),
                 // FadeInImage.memoryNetwork(placeholder: 'assets/images/exercicio.gif', image: 'assets/images/exercicio.gif',),
-                SimpleTextComponent(text: 'Para pacientes em tratamento intensivo, com múltiplas injeções de insulina ou sistema de infusão contínua, a AMGC deve ser feita no mínimo quatro vezes ao dia, geralmente antes e depois das refeições e ao deitar.'),
-                SimpleTextComponent(text:'Auto-monitoramento doméstico de glicose:'),
-                SimpleTextWithPointComponent(text:'rastreia níveis imediatos e diários de controle da glicose;', textSize: 16,),
-                SimpleTextWithPointComponent( text: 'ajuda a determinar insulina basal e bolus imediata e contínua requisitos;'),
-                SimpleTextWithPointComponent( text: 'detecta hipoglicemia e auxilia no seu manejo;'),
-                SimpleTextWithPointComponent( text: 'auxilia no tratamento adequado da hiperglicemia; e'),
-                SimpleTextWithPointComponent( text: 'ajuda a orientar os ajustes de insulina para diminuir as flutuações da glicose.'),
-                
+                SimpleTextComponent(
+                    text:
+                        'Para pacientes em tratamento intensivo, com múltiplas injeções de insulina ou sistema de infusão contínua, a AMGC deve ser feita no mínimo quatro vezes ao dia, geralmente antes e depois das refeições e ao deitar.'),
+                SimpleTextComponent(
+                    text: 'Auto-monitoramento doméstico de glicose:'),
+                SimpleTextWithPointComponent(
+                  text:
+                      'rastreia níveis imediatos e diários de controle da glicose;',
+                  textSize: 16,
+                ),
+                SimpleTextWithPointComponent(
+                    text:
+                        'ajuda a determinar insulina basal e bolus imediata e contínua requisitos;'),
+                SimpleTextWithPointComponent(
+                    text: 'detecta hipoglicemia e auxilia no seu manejo;'),
+                SimpleTextWithPointComponent(
+                    text: 'auxilia no tratamento adequado da hiperglicemia; e'),
+                SimpleTextWithPointComponent(
+                    text:
+                        'ajuda a orientar os ajustes de insulina para diminuir as flutuações da glicose.'),
               ],
             ),
           ),
@@ -34,32 +50,29 @@ class GlicemiaCapilarPage extends StatelessWidget {
     );
   }
 
-
-Widget imageDetails2(context, String image, [String tagname, String text]) {
-  return Center(
-    child: Column(
-      children: <Widget>[
-        GestureDetector(
-          child: Hero(
-            tag: tagname,
-            child: Image.asset(image,
-                width: MediaQuery.of(context).size.width ,
-                height: MediaQuery.of(context).size.height * 0.5),
+  Widget imageDetails2(context, String image, [String tagname, String text]) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          GestureDetector(
+            child: Hero(
+              tag: tagname,
+              child: Image.asset(image,
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height * 0.5),
+            ),
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return ImageDetails('Exercício', '', image, tagname);
+              }));
+            },
           ),
-          onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) {
-              return ImageDetails('Exercício', '', image, tagname);
-            }));
-          },
-        ),
-         Text(text != null ? text : '',
-            style: TextStyle(
-              fontSize: 11,
-            ))
-      ],
-    ),
-  );
-}
-
-
+          Text(text != null ? text : '',
+              style: TextStyle(
+                fontSize: 11,
+              ))
+        ],
+      ),
+    );
+  }
 }
